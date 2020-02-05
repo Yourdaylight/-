c8b25148f14ea320e79b87e8c89d4c080ea98a8e
Yourdaylight <52725224+Yourdaylight@users.noreply.github.com>
2020-02-05 20:18:13 +0800

    Update 摆动序列.java

--- 算法训练/摆动序列.java
+++ 算法训练/摆动序列.java
@@ -2,15 +2,15 @@ package AlgorithmTrainning;
 
 import java.util.Scanner;
 
-public class �ڶ�����{
-	static int[] value;//�������ֵ
-	static int[] isfind;//��Ǳ������ĵ㣨��1 ��ʾ��
+public class 摆动序列{
+	static int[] value;//存放序列值
+	static int[] isfind;//标记遍历过的点（用1 表示）
 	static int sum=0;
 	public static int k;
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-		k=sc.nextInt();//���г���[1,k]
+		k=sc.nextInt();//序列长度[1,k]
 		value=new int[k];
 		isfind=new int[k+1];
 		
@@ -21,8 +21,8 @@ public class �ڶ�����{
 
 	
 	public static void dfs(int n)
+	//n:value的索引值（长度为n的序列）
 	{
-
 		if(n>1)
 		{
 			if(n==2)
@@ -39,9 +39,11 @@ public class �ڶ�����{
 
 			if(isfind[i]==0)
 			{
-				value[n]=i;//�ҵ�iδ�����������������еĵ�n��ֵ
-				isfind[i]=1;//�ҵ�Ԫ�ؾ���1
-				dfs(n+1);
+				value[n]=i;//找到i未遍历，将其存放在序列的第n个值
+				isfind[i]=1;//找到元素就置1
+				dfs(n+1);//寻找序列的下一个值
+				
+				//回溯
 				value[n]=0;
 				isfind[i]=0;
 			}
